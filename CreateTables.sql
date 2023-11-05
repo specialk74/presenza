90f8b6834dbdc88a5be4294c1e3aef8f679bb33c
Marco Faletra <m.faletra@gmail.com>
2023-11-05 19:52:36 +0100

    Changes on update

--- CreateTables.sql
+++ CreateTables.sql
@@ -12,9 +12,9 @@ CREATE TABLE presenza (
 	lastupdate DATE NOT NULL
 );
 
-INSERT INTO presenza (name, pranzo, cena, dormire, pranzoDefault, cenaDefault, dormireDefault, lastupdate) VALUES ("Marco",   0,0,0,0,1,1, datetime());
-INSERT INTO presenza (name, pranzo, cena, dormire, pranzoDefault, cenaDefault, dormireDefault, lastupdate) VALUES ("Simona",  0,0,0,0,1,1, datetime());
+INSERT INTO presenza (name, pranzo, cena, dormire, pranzoDefault, cenaDefault, dormireDefault, lastupdate) VALUES ("Marco",   0,0,0,96,127,127, datetime());
+INSERT INTO presenza (name, pranzo, cena, dormire, pranzoDefault, cenaDefault, dormireDefault, lastupdate) VALUES ("Simona",  0,0,0,96,127,127, datetime());
 INSERT INTO presenza (name, pranzo, cena, dormire, pranzoDefault, cenaDefault, dormireDefault, lastupdate) VALUES ("Lucia",   0,0,0,0,0,0, datetime());
 INSERT INTO presenza (name, pranzo, cena, dormire, pranzoDefault, cenaDefault, dormireDefault, lastupdate) VALUES ("Gabriele",0,0,0,0,0,0, datetime());
 INSERT INTO presenza (name, pranzo, cena, dormire, pranzoDefault, cenaDefault, dormireDefault, lastupdate) VALUES ("Paolo",   0,0,0,0,0,0, datetime());
-INSERT INTO presenza (name, pranzo, cena, dormire, pranzoDefault, cenaDefault, dormireDefault, lastupdate) VALUES ("Michele", 0,0,0,0,1,1, datetime());
+INSERT INTO presenza (name, pranzo, cena, dormire, pranzoDefault, cenaDefault, dormireDefault, lastupdate) VALUES ("Michele", 0,0,0,96,127,127, datetime());
--- update.py
+++ update.py
@@ -1,4 +1,5 @@
 import sqlite3
+from datetime import datetime
 
 connection = sqlite3.connect('database.db')
 connection.row_factory = sqlite3.Row
@@ -8,16 +9,23 @@ rows = connection.execute('SELECT * FROM presenza ORDER BY id').fetchall()
 for row in rows:
  print(row['cenaDefault'])
 
+ d = datetime.now()
+ 
+ if (d.weekday() == 0):
+  shiftter = 6
+ else:
+  shiftter = d.weekday() - 1
+  
  newPranzo = row['pranzo'] >> 1
- if row['pranzoDefault'] != 0:
+ if (row['pranzoDefault'] & (1 << shiftter)) != 0:
    newPranzo = newPranzo | (1 << 6)
 
  newCena = row['cena'] >> 1
- if row['cenaDefault'] != 0:
+ if (row['cenaDefault'] & (1 << shiftter)) != 0:
    newCena = newCena | (1 << 6)
 
  newDormire = row['dormire'] >> 1
- if row['dormireDefault'] != 0:
+ if (row['dormireDefault'] & (1 << shiftter)) != 0:
   newDormire = newDormire | (1 << 6)
 
  connection.execute('UPDATE presenza SET pranzo=?, cena=?, dormire=? WHERE id=?', (newPranzo, newCena, newDormire, row['id']))
